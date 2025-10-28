e9453c09f288027395fc4958139e0fc51f1cd48a
Justin Hou <justinhou2005@gmail.com>
2025-10-27 21:42:48 -0400

    reset

--- models/__init__.py
+++
@@ -1,2 +0,0 @@
-# Models package for fire detection
-
--- test.py
+++
@@ -1,6 +0,0 @@
-import kagglehub
-
-# Download latest version
-path = kagglehub.dataset_download("pengbo00/home-fire-dataset")
-
-print("Path to dataset files:", path)
